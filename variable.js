355ec4f26e93a7197b3f501214bab3c305145adf
ahdianmirza <ahdianmirza@gmail.com>
2023-01-06 14:53:27 +0700

    menambahkan latihan javascript variable

---
+++ variable.js
@@ -0,0 +1,17 @@
+// cara pertama
+let lastName;
+lastName = "Mirza Azri";
+
+console.log(lastName);
+
+
+// cara kedua
+let lastName2 = 'Ahdian';
+console.log(lastName2);
+
+// const variable
+const z = 100;
+console.log(z);
+
+z = 200;
+console.log(z) // error
