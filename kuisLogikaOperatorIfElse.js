15705a731891a8c4a789f6b8b51d0037fff4269c
ahdianmirza <ahdianmirza@gmail.com>
2023-01-06 16:45:21 +0700

    menambahkan kuis javascript logika operator dan if else

---
+++ kuisLogikaOperatorIfElse.js
@@ -0,0 +1,15 @@
+let score = 55;
+
+if (score >= 90) {
+    result = 'Selamat! Anda mendapatkan nilai A.';
+} else if (score >= 80 && score < 90) {
+    result = 'Anda mendapatkan nilai B.';
+} else if (score >= 70 && score < 80) {
+    result = 'Anda mendapatkan nilai C.';
+} else if (score >= 60 && score < 70) {
+    result = 'Anda mendapatkan nilai D.';
+} else if (score < 60) {
+    result = 'Anda mendapatkan nilai E.';
+}
+
+console.log(result);
--- kuisVariableTipeData.js
+++
@@ -1,4 +0,0 @@
-let firstName = 'Ahdian';
-let lastName = 'Azri';
-let age = 21;
-let isMarried = false;
