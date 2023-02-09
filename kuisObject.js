92264ce3805c38828faf8c423d3c3fef7763f83a
ahdianmirza <ahdianmirza@gmail.com>
2023-02-10 03:40:34 +0700

    menambahkan file latihan kuis object

---
+++ kuisObject.js
@@ -0,0 +1,10 @@
+const restaurant = {
+    name: "Warung Garasi",
+    city: "Banyuwangi",
+    "favorite drink": "Cappucino",
+    "favorite food": "Kentang Goreng",
+    isVegan: false,
+};
+
+const {name} = restaurant;
+const favoriteDrink = restaurant["favorite drink"];
